336ee3f38ebe914046f00fcd790e50d2311112c3
unknown <monkey03297@gmail.com>
2025-11-12 16:41:34 +0800

    test2

--- js/showcase.js
+++ js/showcase.js
@@ -50,7 +50,7 @@ function prevImage(){
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-var grd = ctx.createLinearGradient(0, 0, 0, 448);
+var grd = ctx.createLinearGradient(0, 0, 0, 400);
 grd.addColorStop(0, "#000099");
 grd.addColorStop(1, "#000000");
 ctx.fillStyle = grd;
@@ -62,7 +62,7 @@ ctx.fillStyle = "black";
 ctx.fillRect(0, 320, 512, 128);
 
 
-
+//moon
 ctx.beginPath();
 ctx.fillStyle = "#FFFF00";
 ctx.arc(400, 80, 48, 0, 2 * Math.PI);
@@ -73,6 +73,7 @@ ctx.fillStyle = "#333300FF";
 ctx.arc(407, 75, 40, 0, 2 * Math.PI);
 ctx.fill();
 
+//building
 ctx.beginPath();
 ctx.fillStyle = "#1F1F3F";
 ctx.fillRect(40, 160, 64, 160);
@@ -86,8 +87,7 @@ ctx.fillRect(164, 200, 8, 8);
 ctx.fillRect(244, 120, 8, 8);
 ctx.fillRect(264, 240, 8, 8);
 ctx.fillRect(440, 220, 8, 8);
-
-
+//star
 ctx.fillStyle = "white";
 ctx.arc(50,90, 2, 0, 2 * Math.PI);
 ctx.fill();
@@ -103,8 +103,65 @@ ctx.beginPath();
 ctx.arc(300, 33, 2, 0, 2 * Math.PI);
 ctx.fill();
 
-
 ctx.font = "24px Arial";
 ctx.fillText("Canvars Showcase", 4, 30);
 ctx.font = "16px Arial";
 ctx.fillText("The Night City", 6, 50);
+
+
+var c2 = document.getElementById("myCanvas2");
+var ctx2 = c2.getContext("2d");
+var grd2 = ctx2.createLinearGradient(0, 0, 0, 200);
+grd2.addColorStop(0, "#CC0000");
+grd2.addColorStop(1, "#FF9900");
+ctx2.fillStyle = grd2;
+ctx2.fillRect(0, 0, 512, 320);
+
+ctx2.beginPath();
+ctx2.fillStyle = "#FFFF00";
+ctx2.arc(256, 200, 48, 0, 2*Math.PI);
+ctx2.fill();
+
+ctx2.beginPath();
+grd2 = ctx2.createLinearGradient(0, 200, 0, 320);
+grd2.addColorStop(0, "#9966FF66");
+grd2.addColorStop(0.4, "#0000CCCC");
+ctx2.fillStyle = grd2;
+ctx2.fillRect(0, 200, 512, 120);
+
+ctx2.fillStyle="#CCCCCC99";
+ctx2.fillRect(50, 100, 64, 16);
+ctx2.beginPath();
+ctx2.arc(50, 108, 8, 0.5*Math.PI, 1.5*Math.PI);
+ctx2.fill();
+ctx2.beginPath();
+ctx2.arc(114, 108, 8, -0.5*Math.PI, 0.5*Math.PI);
+ctx2.fill();
+
+ctx2.fillRect(350, 150, 64, 16);
+ctx2.beginPath();
+ctx2.arc(350, 158, 8, 0.5*Math.PI, 1.5*Math.PI);
+ctx2.fill();
+ctx2.beginPath();
+ctx2.arc(414, 158, 8, -0.5*Math.PI, 0.5*Math.PI);
+ctx2.fill();
+
+ctx2.fillRect(400, 50, 32, 16);
+ctx2.beginPath();
+ctx2.arc(400, 58, 8, 0.5*Math.PI, 1.5*Math.PI);
+ctx2.fill();
+ctx2.beginPath();
+ctx2.arc(432, 58, 8, -0.5*Math.PI, 0.5*Math.PI);
+ctx2.fill();
+
+/*
+ctx2.beginPath();
+ctx2.moveTo(30, 220);
+ctx2.lineTo(250, 220);
+ctx2.lineTo(230, 300);
+ctx2.lineTo(50,300);
+ctx2.lineTo(30,220);
+ctx2.stroke();
+ctx2.fillStyle = "#666633";
+ctx2.fill();
+*/
